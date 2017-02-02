717389962f97bfb5a2952512a40c1c3f3b822922
YuriyOrlov <navuchodonsr@yandex.ru>
2017-02-02 11:50:06 +0300

    Changed function for converting complexity bits into points. Fixed function for bonus points check.

--- password_strength.py
+++ password_strength.py
@@ -20,9 +20,9 @@ def check_characters(password):
         return 0.5
 
 
-def get_bonus_points(password, common_passw_list=None):
+def get_complexity_bonus_points(password, common_passw_list=None):
     if common_passw_list:
-        blacklist_password_check = True
+        blacklist_password_check = False
         blacklist_password_check = password not in common_passw_list
         lower_upper_characters = check_characters(password)
         spec_character_check = any(char for char in password if char in punctuation)
@@ -33,33 +33,30 @@ def get_bonus_points(password, common_passw_list=None):
     return sum([num for num in (blacklist_password_check, lower_upper_characters, spec_character_check)])
 
 
-def get_password_strength(password, bonus_points=0):
-    alphabetSize = len(set(password)) + 1 + bonus_points
-    count_bits = int(log2(alphabetSize) * len(password))
-    if count_bits <= 5:
-        return 1
-    elif 6 <= count_bits <= 14:
-        return 2
-    elif 15 <= count_bits <= 19:
-        return 3
-    elif 20 <= count_bits <= 24:
-        return 4
-    elif 25 <= count_bits <= 29:
-        return 5
-    elif 30 <= count_bits <= 35:
-        return 6
-    elif 36 <= count_bits <= 39:
-        return 7
-    elif 40 <= count_bits <= 45:
-        return 8
-    elif 46 <= count_bits <= 49:
-        return 9
-    elif count_bits >= 50:
-        return 10
+def get_password_strength_in_bits(password, bonus_points=0):
+    count_unique_chars = len(set(password)) + 1 + bonus_points
+    return int((log2(count_unique_chars) * len(password)) / 2)
+
+
+def convert_bits_to_points(number_of_bits):
+    ranges = {1: (1, 6),
+              2: (6, 14),
+              3: (14, 19),
+              4: (19, 24),
+              5: (24, 29),
+              6: (29, 35),
+              7: (35, 39),
+              8: (39, 45),
+              9: (45, 50),
+              10: (50, 1000)}
+    for key, value in ranges.items():
+        if number_of_bits in range(value[0], value[1]):
+            return key
 
 
 if __name__ == '__main__':
     password_list_from_text = load_password_list('./most_common_passwords.txt')
-    users_password = getpass.getpass(prompt='\nEnter your password> ', stream=None)
-    get_bonus_for_complexity = get_bonus_points(users_password, password_list_from_text)
-    print(get_password_strength(users_password, get_bonus_for_complexity))
+    user_password = getpass.getpass(prompt='\nEnter your password> ', stream=None)
+    get_bonus_for_complexity = get_complexity_bonus_points(user_password, password_list_from_text)
+    get_password_complexity = get_password_strength_in_bits(user_password, get_bonus_for_complexity)
+    print(convert_bits_to_points(get_password_complexity))
