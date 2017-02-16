c3ab54e893bc16d575b598032e33fcf90b761862
YuriyOrlov <navuchodonsr@yandex.ru>
2017-02-16 11:34:16 +0300

    Added new function - now user can point which TXT file with "bad" passwords to use. Added arguments parser to the program and made some minor bugfixes.

--- password_strength.py
+++ password_strength.py
@@ -1,40 +1,85 @@
 import getpass
+import argparse
+import textwrap
+import sys
 from os import path
 from math import log2
 from string import punctuation
+from chardet import detect
+
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: {}\n'.format(message))
+        self.print_help()
+        sys.exit(2)
+
+    def check_python_version(self):
+        if sys.version_info < (3, 5):
+            self.print_help()
+            raise SystemExit('\nSorry, this code needs Python 3.5 or higher\n')
+
+
+def create_parser():
+    parser = MyParser(prog='Password complexity', formatter_class=argparse.RawDescriptionHelpFormatter,
+                      description=textwrap.dedent('''\
+                      Script for password complexity check \n
+                      -----------------------------------------------------------------
+                      This script needs file for better work:
+                      *most_common_passwords.txt - list of bad passwords according to \
+                                                 the investigations of data breaches \
+                                                and hack attacks in 2015.
+
+                      If you want to stop the program press Ctrl+C.
+                      ------------------------------------------------------------------
+                      This program had been tested on Python 3.5.2.
+                      '''))
+    parser.add_argument('--file', nargs='?',
+                        help='Paste full path to file with bad passwords,\
+                              e.g --file /home/user/documents/most_common_passwords.txt\
+                              (default: %(default)s)',
+                        type=str, default=None)
+    return parser
+
+
+def detect_encoding(filepath):
+    with open(filepath, 'br') as file:
+        raw_data = file.read()
+    return detect(raw_data)['encoding']
 
 
 def load_password_list(filepath):
+    possible_encoding = detect_encoding(filepath)
     if not path.exists(filepath):
         return None
-    with open(filepath, 'r', encoding='cp1251') as file:
+    with open(filepath, 'r', encoding=possible_encoding) as file:
         return [line.strip() for line in file.readlines()]
 
 
-def check_characters(password):
-    upper_case_check = any(char.isupper() for char in password)
-    lower_case_check = any(char.islower() for char in password)
-    if all((upper_case_check, lower_case_check)):
+def acquire_bonus_points_for_characters(password):
+    is_chars_upper_case = any(char.isupper() for char in password)
+    is_chars_lower_case = any(char.islower() for char in password)
+    if all((is_chars_upper_case, is_chars_lower_case)):
         return 1.0
     else:
         return 0.5
 
 
-def get_complexity_bonus_points(password, common_passw_list=None):
+def gain_complexity_bonus_points(password, common_passw_list=None):
     if common_passw_list:
-        blacklist_password_check = False
-        blacklist_password_check = password not in common_passw_list
-        lower_upper_characters = check_characters(password)
+        blacklist_password_check = 1.0 if password not in common_passw_list else 0
+        lower_upper_characters = acquire_bonus_points_for_characters(password)
         spec_character_check = any(char for char in password if char in punctuation)
     else:
-        blacklist_password_check = False
-        lower_upper_characters = check_characters(password)
+        blacklist_password_check = 0
+        lower_upper_characters = acquire_bonus_points_for_characters(password)
         spec_character_check = any(char for char in password if char in punctuation)
     return sum([num for num in (blacklist_password_check, lower_upper_characters, spec_character_check)])
 
 
-def get_password_strength_in_bits(password, bonus_points=0):
-    count_unique_chars = len(set(password)) + 1 + bonus_points
+def get_password_strength_in_bits(password, password_list_from_text=None):
+    get_bonus_for_complexity = gain_complexity_bonus_points(user_password, password_list_from_text)
+    count_unique_chars = len(set(password)) + 1 + get_bonus_for_complexity
     return int((log2(count_unique_chars) * len(password)) / 2)
 
 
@@ -55,8 +100,10 @@ def convert_bits_to_points(number_of_bits):
 
 
 if __name__ == '__main__':
-    password_list_from_text = load_password_list('./most_common_passwords.txt')
+    parser = create_parser()
+    parser.check_python_version()
+    args = parser.parse_args()
+    password_list_from_text = load_password_list(args.file) if args.file else None
     user_password = getpass.getpass(prompt='\nEnter your password> ', stream=None)
-    get_bonus_for_complexity = get_complexity_bonus_points(user_password, password_list_from_text)
-    get_password_complexity = get_password_strength_in_bits(user_password, get_bonus_for_complexity)
+    get_password_complexity = get_password_strength_in_bits(user_password, password_list_from_text)
     print(convert_bits_to_points(get_password_complexity))
