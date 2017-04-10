b75ae2e22ec2afbb10ffeda91bb2869cdcbf1620
YuriyOrlov <navuchodonsr@yandex.ru>
2017-04-10 15:11:13 +0300

    Changed script for better readability.

--- password_strength.py
+++ password_strength.py
@@ -6,9 +6,9 @@ from os import path
 from math import log2
 from string import punctuation
 
-RANGES_FOR_CONVERTING_BITS = [(1, 6), (6, 14), (14, 19), (19, 24),
-                              (24, 29), (29, 35), (35, 39), (39, 45),
-                              (45, 50), (50, 1000)]
+ENTROPY_BITS = [(1, 6), (6, 14), (14, 19), (19, 24),
+                (24, 29), (29, 35), (35, 39), (39, 45),
+                (45, 50), (50, 1000)]
 
 
 class MyParser(argparse.ArgumentParser):
@@ -54,28 +54,28 @@ def load_password_list(filepath):
 
 def get_password_strength_in_bits(password, bad_passwords_list=None):
     if bad_passwords_list:
-        blacklist_password_check = 1.0 if password not in bad_passwords_list else 0
-        any_char_is_in_upper_case = any(char.isupper() for char in password)
-        any_char_is_in_lower_case = any(char.islower() for char in password)
-        bonus_for_lower_or_upper_case_character = 1.0 if all((any_char_is_in_upper_case,
-                                                              any_char_is_in_lower_case)) else 0.5
-        spec_character_check = any(char for char in password if char in punctuation)
+        password_in_blacklist = True if password not in bad_passwords_list else False
+        has_uppercase_chars = any(char.isupper() for char in password)
+        has_lowercase_chars = any(char.islower() for char in password)
+        letter_case_bonus = 1.0 if all((has_uppercase_chars,
+                                        has_lowercase_chars)) else 0.5
+        has_special_chars = any(char for char in password if char in punctuation)
     else:
-        blacklist_password_check = 0
-        any_char_is_in_upper_case = any(char.isupper() for char in password)
-        any_char_is_in_lower_case = any(char.islower() for char in password)
-        bonus_for_lower_or_upper_case_character = 1.0 if all((any_char_is_in_upper_case,
-                                                              any_char_is_in_lower_case)) else 0.5
-        spec_character_check = any(char for char in password if char in punctuation)
-    bonus_points_for_complexity = sum([num for num in (blacklist_password_check,
-                                                       bonus_for_lower_or_upper_case_character,
-                                                       spec_character_check)])
-    count_unique_chars = len(set(password)) + 1 + bonus_points_for_complexity
-    return int((log2(count_unique_chars) * len(password)) / 2)
+        password_in_blacklist = False
+        has_uppercase_chars = any(char.isupper() for char in password)
+        has_lowercase_chars = any(char.islower() for char in password)
+        letter_case_bonus = 1.0 if all((has_uppercase_chars,
+                                        has_lowercase_chars)) else 0.5
+        has_special_chars = any(char for char in password if char in punctuation)
+    bonus_points = sum([num for num in (password_in_blacklist,
+                                        letter_case_bonus,
+                                        has_special_chars)])
+    complexity_points = len(set(password)) + 1 + bonus_points
+    return int((log2(complexity_points) * len(password)) / 2)
 
 
-def convert_bits_to_points(number_of_bits, RANGES_FOR_CONVERTING_BITS=RANGES_FOR_CONVERTING_BITS):
-    return [index for index, bits_range in enumerate(RANGES_FOR_CONVERTING_BITS)
+def convert_bits_to_points(number_of_bits, ENTROPY_BITS=ENTROPY_BITS):
+    return [index for index, bits_range in enumerate(ENTROPY_BITS)
             if number_of_bits in range(*bits_range)]
 
 
